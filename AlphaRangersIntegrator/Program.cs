a804e056864b1e9f19cde9e30d7c427142d02a67
Lucaspeta <lucas.ferreira.13@hotmail.com>
2019-10-26 17:21:33 -0300

    Add project files.

---
+++ AlphaRangersIntegrator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO.Ports;
+using System.Threading;
+
+namespace AlphaRangersIntegrator
+{
+    class Program
+    {  
+        static SerialPort _serialPort;
+        public static void Main()
+        {
+            _serialPort = new SerialPort
+            {
+                PortName = "COM4",
+                BaudRate = 9600
+            };
+
+            if(!_serialPort.IsOpen)
+                _serialPort.Open();
+
+            while (true)
+            {
+                string a = _serialPort.ReadExisting();
+                Console.WriteLine(a);
+                Thread.Sleep(200);
+            }
+        }
+    }
+}
